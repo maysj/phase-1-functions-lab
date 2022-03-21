da7712040240a210963554f177dca42cc8bda4c7
Mays Jones <maysjones2@gmail.com>
2022-03-21 13:40:32 -0400

    commit tree dfd2e61ff93088557d982ae605aff944365329ad

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+return Math.abs(pickup-42)
+}
+function distanceFromHqInFeet(someValue) {
+    return (distanceFromHqInBlocks(someValue)*264);
+}
+
+
+
+function distanceTravelledInFeet(start, destination){
+return (Math.abs(start-destination))*264
+}
+
+function calculatesFarePrice(start, destination){
+    if ((Math.abs(start-destination))*264 <= 400) 
+    return 0
+    else if ((Math.abs(start-destination))*264 <= 2000)
+    return ((((Math.abs(start-destination))*264)-400)*.02)
+    else if ((Math.abs(start-destination))*264 <= 2500)
+    return 25
+    else if ((Math.abs(start-destination))*264 >= 2500)
+    return 'cannot travel that far'
+}
